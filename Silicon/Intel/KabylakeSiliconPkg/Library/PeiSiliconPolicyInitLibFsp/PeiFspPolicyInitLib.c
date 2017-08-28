d8a19e493e1b7ff2c86b253abf480cf814c69293
Jiewen Yao <jiewen.yao@intel.com>
2017-08-28 09:37:58 +0800

    new policy design for silicon.
    Cc: Michael A Kubacki <michael.a.kubacki@intel.com>
    Cc: Amy Chan <amy.chan@intel.com>
    Cc: Chasel Chiu <chasel.chiu@intel.com>
    Cc: Brett Wang  <brett.wang@intel.com>
    Cc: Daocheng Bu <daocheng.bu@intel.com>
    Cc: Isaac W Oram <isaac.w.oram@intel.com>
    Cc: Rangasai V Chaganty <rangasai.v.chaganty@intel.com>
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Jiewen Yao <jiewen.yao@intel.com>
    Reviewed-by: Michael A Kubacki <michael.a.kubacki@intel.com>
    Reviewed-by: Amy Chan <amy.chan@intel.com>

--- Silicon/Intel/KabylakeSiliconPkg/Library/PeiSiliconPolicyInitLibFsp/PeiFspPolicyInitLib.c
+++ Silicon/Intel/KabylakeSiliconPkg/Library/PeiSiliconPolicyInitLibFsp/PeiFspPolicyInitLib.c
@@ -15,16 +15,29 @@ WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 #include <PeiFspPolicyInitLib.h>
 
 /**
-  Performs FSPM UPD Policy initialization.
+  Performs silicon pre-mem policy initialization.
 
+  The meaning of Policy is defined by silicon code.
+  It could be the raw data, a handle, a PPI, etc.
+
+  The returned data must be used as input data for SiliconPolicyDonePreMem(),
+  and SiliconPolicyUpdateLib.SiliconPolicyUpdatePreMem().
+
+  1) In FSP path, the input Policy should be FspmUpd.
   Value of FspmUpd has been initialized by FSP binary default value.
   Only a subset of FspmUpd needs to be updated for different silicon sku.
+  The return data is same FspmUpd.
 
-  @param[in, out] FspmUpd       Pointer to FSPM UPD data.
+  2) In non-FSP path, the input policy could be NULL.
+  The return data is the initialized policy.
+
+  @param[in, out] Policy       Pointer to policy.
+
+  @return the initialized policy.
 **/
-VOID
+VOID *
 EFIAPI
-FspmPolicyInit (
+SiliconPolicyInitPreMem (
   IN OUT VOID    *FspmUpd
   )
 {
@@ -84,19 +97,52 @@ FspmPolicyInit (
   //
   Status = PeiFspMiscUpdInitPreMem (FspmUpdDataPtr);
 
+  return FspmUpd;
+}
+
+/*
+  The silicon pre-mem policy is finalized.
+  Silicon code can do initialization based upon the policy data.
+
+  The input Policy must be returned by SiliconPolicyInitPreMem().
+  
+  @param[in] Policy       Pointer to policy.
+
+  @retval EFI_SUCCESS The policy is handled consumed by silicon code.
+*/
+EFI_STATUS
+EFIAPI
+SiliconPolicyDonePreMem (
+  IN OUT VOID    *FspmUpd
+  )
+{
+  return EFI_SUCCESS;
 }
 
 /**
-  Performs FSPS UPD Policy initialization.
+  Performs silicon post-mem policy initialization.
 
+  The meaning of Policy is defined by silicon code.
+  It could be the raw data, a handle, a PPI, etc.
+
+  The returned data must be used as input data for SiliconPolicyDonePostMem(),
+  and SiliconPolicyUpdateLib.SiliconPolicyUpdatePostMem().
+
+  1) In FSP path, the input Policy should be FspsUpd.
   Value of FspsUpd has been initialized by FSP binary default value.
   Only a subset of FspsUpd needs to be updated for different silicon sku.
+  The return data is same FspsUpd.
 
-  @param[in, out] FspsUpd       Pointer to FSPS UPD data.
+  2) In non-FSP path, the input policy could be NULL.
+  The return data is the initialized policy.
+
+  @param[in, out] Policy       Pointer to policy.
+
+  @return the initialized policy.
 **/
-VOID
+VOID *
 EFIAPI
-FspsPolicyInit (
+SiliconPolicyInitPostMem (
   IN OUT VOID    *FspsUpd
   )
 {
@@ -143,4 +189,24 @@ FspsPolicyInit (
     DEBUG ((DEBUG_WARN, "ERROR - CPU Pei Fsp Policy Initialization fail, Status = %r\n", Status));
   }
 
+  return FspsUpd;
+}
+
+/*
+  The silicon post-mem policy is finalized.
+  Silicon code can do initialization based upon the policy data.
+
+  The input Policy must be returned by SiliconPolicyInitPostMem().
+  
+  @param[in] Policy       Pointer to policy.
+
+  @retval EFI_SUCCESS The policy is handled consumed by silicon code.
+*/
+EFI_STATUS
+EFIAPI
+SiliconPolicyDonePostMem (
+  IN OUT VOID    *FspsUpd
+  )
+{
+  return EFI_SUCCESS;
 }
