43378096976333db0901bef685dd3e246392d196
Kalyan Nagabhirava <kalyankumar.nagabhirava@linaro.org>
2018-02-12 16:01:19 +0530

    Platform/Comcast: add RDK boot manager library implementation
    Implemented features related to secure boot and DRI (downloading the image
    and storing on flash), library has utility of file read and write operations
    for fat flash partition, it reads file path and load the file content
    using configuration file.
    
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Moorthy Baskaravenkatraman <moorthy.baskaravenkatraman-sambamoorthy@linaro.org>
    Reviewed-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>

---
+++ Platform/Comcast/Library/RdkBootManagerLib/RdkBootManagerLib.h
@@ -0,0 +1,99 @@
+/*
+#  Copyright (c) 2014-2018, Linaro Limited. All rights reserved.
+#
+#  This program and the accompanying materials
+#  are licensed and made available under the terms and conditions of the BSD License
+#  which accompanies this distribution.  The full text of the license may be found at
+#  http://opensource.org/licenses/bsd-license.php
+#
+#  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+#
+*/
+#ifndef __RDK_BOOT_MANAGER_LIB_H__
+#define __RDK_BOOT_MANAGER_LIB_H__
+
+#include <Library/BdsLib.h>
+#include <Library/UefiLib.h>
+#include <Library/BaseLib.h>
+#include <Library/DebugLib.h>
+#include <Library/PrintLib.h>
+#include <Library/ShellLib.h>
+#include <Library/DevicePathLib.h>
+#include <Library/FileHandleLib.h>
+#include <Library/MemoryAllocationLib.h>
+#include <Library/UefiBootServicesTableLib.h>
+#include <Library/UefiRuntimeServicesTableLib.h>
+#include <Protocol/DiskIo.h>
+#include <Protocol/BlockIo.h>
+#include <Protocol/LoadFile.h>
+#include <Protocol/SimpleTextOut.h>
+#include <Protocol/DevicePathFromText.h>
+#include <Protocol/DevicePathToText.h>
+#include <Protocol/AndroidFastbootPlatform.h>
+#include <Guid/ImageAuthentication.h>
+#include <Guid/AuthenticatedVariableFormat.h>
+#include <HttpBootDxe/HttpBootDxe.h>
+#include <Include/Guid/AuthenticatedVariableFormat.h>
+
+extern
+EFI_STATUS
+PartitionRead (
+  IN CHAR8  *PartitionName,
+  IN VOID   *Image,
+  IN UINTN  Size
+  );
+
+extern
+EFI_STATUS
+PartitionWrite (
+  IN CHAR8  *PartitionName,
+  IN VOID   *Image,
+  IN UINTN  Size
+  );
+
+extern
+EFI_STATUS
+GetRdkVariable (
+  IN  CONST CHAR16  *Name,
+  OUT CONST CHAR16  **Value
+  );
+
+extern
+EFI_STATUS
+RdkSecureBoot (
+  EFI_HANDLE		ImageHandle,
+  EFI_BOOT_SERVICES     *BootServices
+  );
+
+extern
+EFI_STATUS
+RdkReadFile (
+  IN  	CONST CHAR16 	*Path,
+  IN OUT  VOID          **BufferPtr,
+  OUT  	UINTN           *FileSize
+  );
+
+extern
+EFI_STATUS
+RdkWriteFile (
+  IN  	CONST CHAR16    *Path,
+  IN OUT  VOID          **BufferPtr,
+  OUT  	UINTN           *FileSize
+  );
+
+extern
+EFI_STATUS
+GetFileHandler (
+  OUT 	EFI_FILE_HANDLE *FileHandle,
+  IN 	CONST CHAR16    *Path,
+  IN  	UINT64          OpenMode
+  );
+
+extern
+EFI_STATUS
+RdkHttpBoot (
+  VOID
+  );
+
+#endif /* __RDK_BOOT_MANAGER_LIB_H__ */
