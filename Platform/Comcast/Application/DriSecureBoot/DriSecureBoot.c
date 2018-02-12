a628fc23b0af7e82b930b641a57805340fdf8af4
Kalyan Nagabhirava <kalyankumar.nagabhirava@linaro.org>
2018-02-12 16:01:21 +0530

    Platform/Comcast: add RDK Disaster Recovery (DRI) Application
    Application will Download platform code Image (kernel + DTB + rootfs)
    and writes into Flash partition.
    
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Moorthy Baskaravenkatraman <moorthy.baskaravenkatraman-sambamoorthy@linaro.org>
    Reviewed-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>

---
+++ Platform/Comcast/Application/DriSecureBoot/DriSecureBoot.c
@@ -0,0 +1,32 @@
+/*
+#  Copyright (c) 2014-2018, Linaro Limited. All rights reserved.
+#
+#  This program and the accompanying materials
+#  are licensed and made available under the terms and conditions of the BSD License
+#  which accompanies this distribution.  The full text of the license may be found at
+#  http://opensource.org/licenses/bsd-license.php
+#
+#  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+#
+ */
+#include <RdkBootManagerLib.h>
+
+EFI_STATUS
+EFIAPI
+DriSecureBootEntryPoint (
+  IN EFI_HANDLE        ImageHandle,
+  IN EFI_SYSTEM_TABLE  *SystemTable
+  )
+{
+  EFI_STATUS Status;
+
+  Status = RdkHttpBoot ();
+
+  Status = RdkSecureBoot (
+    ImageHandle,
+    SystemTable->BootServices
+    );
+
+  return Status;
+}
