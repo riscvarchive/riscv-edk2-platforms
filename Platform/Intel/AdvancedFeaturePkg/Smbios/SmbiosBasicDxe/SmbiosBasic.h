4d44b9c9f8491cfed6b61ad4be7f1fef15f7fcc0
Jiewen Yao <jiewen.yao@intel.com>
2018-03-08 09:02:49 +0800

    SmbiosBasic: Correct license header.
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Jiewen Yao <jiewen.yao@intel.com>

--- Platform/Intel/AdvancedFeaturePkg/Smbios/SmbiosBasicDxe/SmbiosBasic.h
+++ Platform/Intel/AdvancedFeaturePkg/Smbios/SmbiosBasicDxe/SmbiosBasic.h
@@ -1,22 +1,14 @@
-/**
-  This file contains a 'Sample Driver' and is licensed as such
-  under the terms of your license agreement with Intel or your
-  vendor.  This file may be modified by the user, subject to
-  the additional terms of the license agreement
+/** @file
+  Smbios basic header file.
 
-Copyright (c) 2006 - 2012, Intel Corporation. All rights reserved.<BR>
-This software and associated documentation (if any) is furnished
-under a license and may only be used or copied in accordance
-with the terms of the license. Except as permitted by such
-license, no part of this software or documentation may be
-reproduced, stored in a retrieval system, or transmitted in any
-form or by any means without the express written consent of
-Intel Corporation.
+Copyright (c) 2018, Intel Corporation. All rights reserved.<BR>
+This program and the accompanying materials are licensed and made available under
+the terms and conditions of the BSD License that accompanies this distribution.
+The full text of the license may be found at
+http://opensource.org/licenses/bsd-license.php.
 
-
-  @file SmbiosMisc.h
-
-  Header file for the SmbiosMisc Driver.
+THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 
 **/
 
