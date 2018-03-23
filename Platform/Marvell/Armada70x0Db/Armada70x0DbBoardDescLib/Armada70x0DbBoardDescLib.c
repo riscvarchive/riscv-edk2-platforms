dd506ed0c4a74f1edaa58ec8792a43437af10373
Marcin Wojtas <mw@semihalf.com>
2018-03-23 16:41:53 +0800

    Marvell/Armada70x0Db: Extend board description library with GPIO
    This patch implements ArmadaBoarDescLib library for
    Armada70x0Db comunity board and introduces ArmadaBoardGpioGet
    routine with per-board GPIO expander description.
    
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Marcin Wojtas <mw@semihalf.com>
    Reviewed-by: Leif Lindholm <leif.lindholm@linaro.org>

--- Platform/Marvell/Armada70x0Db/Armada70x0DbBoardDescLib/Armada70x0DbBoardDescLib.c
+++ Platform/Marvell/Armada70x0Db/Armada70x0DbBoardDescLib/Armada70x0DbBoardDescLib.c
@@ -19,7 +19,31 @@
 #include <Library/DebugLib.h>
 #include <Library/IoLib.h>
 #include <Library/MemoryAllocationLib.h>
+#include <Library/MvGpioLib.h>
 #include <Library/UefiBootServicesTableLib.h>
+
+//
+// GPIO Expander
+//
+STATIC MV_GPIO_EXPANDER mGpioExpander = {
+  PCA9555_ID,
+  0x21,
+  0x0,
+};
+
+
+EFI_STATUS
+EFIAPI
+ArmadaBoardGpioExpanderGet (
+  IN OUT MV_GPIO_EXPANDER **GpioExpanders,
+  IN OUT UINTN             *GpioExpanderCount
+  )
+{
+  *GpioExpanderCount = 1;
+  *GpioExpanders = &mGpioExpander;
+
+  return EFI_SUCCESS;
+}
 
 //
 // Order of devices in SdMmcDescTemplate has to be in par with ArmadaSoCDescLib
