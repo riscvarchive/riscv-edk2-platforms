kettenis/edk2-open-platform

Marvell/Drivers: MvBoardDesc: Extend protocol with MDIO support
Introduce new callback that can provide information
about MDIO controllers to the Mdio driver.

Extend ArmadaBoardDescLib with new structure MV_BOARD_MDIO_DESC,
for holding board specific data.

Contributed-under: TianoCore Contribution Agreement 1.1
Signed-off-by: Marcin Wojtas <mw@semihalf.com>
Reviewed-by: Hua Jing <jinghua@marvell.com>

Silicon/Marvell/Include/Library/ArmadaBoardDescLib.h
@@ -28,6 +28,14 @@ typedef struct {
 } MV_BOARD_COMPHY_DESC;
 
 //
+// MDIO devices per-board description
+//
+typedef struct {
+  MV_SOC_MDIO_DESC *SoC;
+  UINT8             MdioDevCount;
+} MV_BOARD_MDIO_DESC;
+
+//
 // NonDiscoverableDevices per-board description
 //
 

riscvarchive/riscv-edk2-platforms

Reviewed-by: Leif Lindholm <leif.lindholm@linaro.org>

Silicon/Marvell/Include/Library/ArmadaBoardDescLib.h
@@ -25,6 +25,14 @@ typedef struct {
 } MV_BOARD_COMPHY_DESC;
 
 //
+// MDIO devices per-board description
+//
+typedef struct {
+  MV_SOC_MDIO_DESC *SoC;
+  UINTN             MdioDevCount;
+} MV_BOARD_MDIO_DESC;
+
+//
 // NonDiscoverableDevices per-board description
 //
 