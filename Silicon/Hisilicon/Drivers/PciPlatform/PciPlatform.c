2e399dbb3a8cd1aab96ef6ec828308228d523ef9
Ming Huang <ming.huang@linaro.org>
2018-07-19 16:56:02 +0800

    Hisilicon/Pci: Add two api for PciPlatform driver
    PciPlatform may add some features platform special, like
    InitAtu and EnlargeAtu for Hi161x, MaxPlayload for Hi1620.
    So Add two api for expansibility.
    
    This patch is to prepare moving EnlargeAtuConfig0() out of
    PciHostBridge. Since the function was originally called in
    NotifyPhase() of phase EfiPciHostBridgeEndEnumeration, so we propose
    to move it to EFI_PCI_PLATFORM_PROTOCOL->PlatformNotify().
    
    To reduce redundant ATU definitions, we also move InitAtu to
    PciPlatformLib in Hi161x.
    
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Ming Huang <ming.huang@linaro.org>
    Signed-off-by: Heyi Guo <heyi.guo@linaro.org>
    Reviewed-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>

--- Silicon/Hisilicon/Drivers/PciPlatform/PciPlatform.c
+++ Silicon/Hisilicon/Drivers/PciPlatform/PciPlatform.c
@@ -103,6 +103,48 @@ PCI_OPTION_ROM_TABLE      mPciOptionRomTable[] = {
   }
 };
 
+/*++
+
+ Routine Description:
+
+   Perform Platform initialization first in PciPlatform.
+
+   Arguments:
+
+   Returns:
+
+    VOID.
+
+--*/
+VOID
+EFIAPI
+PciInitPlatform (
+  VOID
+  );
+
+/*++
+
+ Routine Description:
+
+   Perform Platform initialization by the phase indicated.
+
+   Arguments:
+
+   HostBridge    -  The associated PCI host bridge handle.
+   Phase         -  The phase of the PCI controller enumeration.
+   ChipsetPhase  -  Defines the execution phase of the PCI chipset driver.
+
+   Returns:
+
+--*/
+VOID
+EFIAPI
+PhaseNotifyPlatform (
+  IN  EFI_HANDLE                                     HostBridge,
+  IN  EFI_PCI_HOST_BRIDGE_RESOURCE_ALLOCATION_PHASE  Phase,
+  IN  EFI_PCI_CHIPSET_EXECUTION_PHASE                ChipsetPhase
+  );
+
 EFI_STATUS
 EFIAPI
 GetPlatformPolicy (
@@ -387,6 +429,7 @@ Returns:
 
 --*/
 {
+  PhaseNotifyPlatform (HostBridge, Phase, ChipsetPhase);
 
   return EFI_SUCCESS;
 }
@@ -414,6 +457,8 @@ Returns:
 {
   EFI_STATUS  Status;
   PCI_PLATFORM_PRIVATE_DATA *PciPrivateData;
+
+  PciInitPlatform ();
 
   PciPrivateData = AllocateZeroPool (sizeof (PCI_PLATFORM_PRIVATE_DATA));
   mPciPrivateData = PciPrivateData;
