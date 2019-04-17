af08f2f4d4a3ace7af57ad7293db6b0dfdbfd774
Marcin Wojtas <mw@semihalf.com>
2019-04-17 11:31:38 +0200

    Marvell/Armada7k8k: Implement PMU interrupt handling
    Generic handling of the PMU interrupts in UEFI and Linux with
    ACPI require enabling a dedicated handler in the EL3.
    Extend the PlatInitDxe with enabler code.
    
    Because for DT world the EL3 service must remain disabled,
    switch it off in the ExitBootServicesEvent. Its execution
    depends on the gEdkiiPlatformHasAcpiGuid status check in the new
    gEfiEventReadyToBootGuid routine.
    
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Marcin Wojtas <mw@semihalf.com>
    Reviewed-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>

--- Silicon/Marvell/Include/IndustryStandard/MvSmc.h
+++ Silicon/Marvell/Include/IndustryStandard/MvSmc.h
@@ -20,5 +20,7 @@
 #define MV_SMC_ID_COMPHY_POWER_OFF        0x82000002
 #define MV_SMC_ID_COMPHY_PLL_LOCK         0x82000003
 #define MV_SMC_ID_DRAM_SIZE               0x82000010
+#define MV_SMC_ID_PMU_IRQ_ENABLE          0x82000012
+#define MV_SMC_ID_PMU_IRQ_DISABLE         0x82000013
 
 #endif //__MV_SMC_H__
