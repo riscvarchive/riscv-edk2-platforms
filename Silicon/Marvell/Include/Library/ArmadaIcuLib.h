918e3c62a2ed592b65475d9426be17e82d12e0da
Michael D Kinney <michael.d.kinney@intel.com>
2019-05-15 15:49:30 -0700

    Silicon/Marvell: Replace BSD License with BSD+Patent License
    https://bugzilla.tianocore.org/show_bug.cgi?id=1373
    
    Replace BSD 2-Clause License with BSD+Patent License.  This change is
    based on the following emails:
    
      https://lists.01.org/pipermail/edk2-devel/2019-February/036260.html
      https://lists.01.org/pipermail/edk2-devel/2018-October/030385.html
    
    RFCs with detailed process for the license change:
    
      V3: https://lists.01.org/pipermail/edk2-devel/2019-March/038116.html
      V2: https://lists.01.org/pipermail/edk2-devel/2019-March/037669.html
      V1: https://lists.01.org/pipermail/edk2-devel/2019-March/037500.html
    
    NOTE: Files with a BSD 3-Clause license are not modified by this patch
          series.
    
    Cc: Leif Lindholm <leif.lindholm@linaro.org>
    Cc: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Michael D Kinney <michael.d.kinney@intel.com>
    Reviewed-by: Leif Lindholm <leif.lindholm@linaro.org>

--- Silicon/Marvell/Include/Library/ArmadaIcuLib.h
+++ Silicon/Marvell/Include/Library/ArmadaIcuLib.h
@@ -2,13 +2,7 @@
 *
 *  Copyright (C) 2018, Marvell International Ltd. and its affiliates
 *
-*  This program and the accompanying materials are licensed and made available
-*  under the terms and conditions of the BSD License which accompanies this
-*  distribution. The full text of the license may be found at
-*  http://opensource.org/licenses/bsd-license.php
-*
-*  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
-*  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+*  SPDX-License-Identifier: BSD-2-Clause-Patent
 *
 **/
 #ifndef __ARMADA_ICU_LIB_H__
