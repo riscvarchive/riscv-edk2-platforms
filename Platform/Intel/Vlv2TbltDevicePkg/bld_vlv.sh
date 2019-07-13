lcp/edk2-platforms

Vlv2TbltDevicePkg/bld_vlv.sh: Update the gcc version detection
Borrow the logic from OvmfPkg/build.sh

Signed-off-by: Gary Lin <glin@suse.com>

Platform/Intel/Vlv2TbltDevicePkg/bld_vlv.sh
@@ -171,12 +171,19 @@ sed -i '/^MAX_CONCURRENT_THREAD_NUMBER/d' Conf/target.txt
 
 gcc_version=$(gcc -v 2>&1 | tail -1 | awk '{print $3}')
 case $gcc_version in
-    4.9.*|4.1[0-9].*|5.*.*|6.*.*)
-      TARGET_TOOLS=GCC49
-      ;;
-    *)
-      TARGET_TOOLS=GCC48
-      ;;
+      [1-3].*|4.[0-7].*)
+        echo OvmfPkg requires GCC4.8 or later
+        exit 1
+        ;;
+      4.8.*)
+        TARGET_TOOLS=GCC48
+        ;;
+      4.9.*|6.[0-2].*)
+        TARGET_TOOLS=GCC49
+        ;;
+      *)
+        TARGET_TOOLS=GCC5
+        ;;
 esac
 
 ACTIVE_PLATFORM=$PLATFORM_PACKAGE/PlatformPkgGcc"$Arch".dsc

riscvarchive/riscv-edk2-platforms

Cc: Zailiang Sun <zailiang.sun@intel.com>
Cc: Yi Qian <yi.qian@intel.com>
Cc: Michael D Kinney <michael.d.kinney@intel.com>
Reviewed-by: Zailiang Sun <zailiang.sun@intel.com>
Reviewed-by: Michael D Kinney <michael.d.kinney@intel.com>

Platform/Intel/Vlv2TbltDevicePkg/bld_vlv.sh
@@ -170,12 +170,19 @@ sed -i '/^MAX_CONCURRENT_THREAD_NUMBER/d' Conf/target.txt
 
 gcc_version=$(gcc -v 2>&1 | tail -1 | awk '{print $3}')
 case $gcc_version in
-    4.9.*|4.1[0-9].*|5.*.*|6.*.*)
-      TARGET_TOOLS=GCC49
-      ;;
-    *)
-      TARGET_TOOLS=GCC48
-      ;;
+      [1-3].*|4.[0-7].*)
+        echo MNW2 requires GCC4.8 or later
+        exit 1
+        ;;
+      4.8.*)
+        TARGET_TOOLS=GCC48
+        ;;
+      4.9.*|6.[0-2].*)
+        TARGET_TOOLS=GCC49
+        ;;
+      *)
+        TARGET_TOOLS=GCC5
+        ;;
 esac
 
 ACTIVE_PLATFORM=$PLATFORM_PACKAGE/PlatformPkgGcc"$Arch".dsc