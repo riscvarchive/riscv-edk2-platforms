1de248b5ab3a1efbe5f271504be30dce22e0fb80
Michael Kubacki <michael.a.kubacki@intel.com>
2019-08-16 16:04:05 -0700

    WhiskeylakeOpenBoardPkg/WhiskeylakeURvp: Add headers
    REF:https://bugzilla.tianocore.org/show_bug.cgi?id=2083
    
    Header files for the WhiskeylakeURvp board instance.
    
    Cc: Sai Chaganty <rangasai.v.chaganty@intel.com>
    Cc: Chasel Chiu <chasel.chiu@intel.com>
    Cc: Liming Gao <liming.gao@intel.com>
    Cc: Nate DeSimone <nathaniel.l.desimone@intel.com>
    Cc: Michael D Kinney <michael.d.kinney@intel.com>
    Cc: Ankit Sinha <ankit.sinha@intel.com>
    Signed-off-by: Michael Kubacki <michael.a.kubacki@intel.com>
    Reviewed-by: Chasel Chiu <chasel.chiu@intel.com>
    Reviewed-by: Nate DeSimone <nathaniel.l.desimone@intel.com>
    Reviewed-by: Ankit Sinha <ankit.sinha@intel.com>

---
+++ Platform/Intel/WhiskeylakeOpenBoardPkg/WhiskeylakeURvp/Include/WhiskeylakeURvpId.h
@@ -0,0 +1,12 @@
+/** @file
+
+  Copyright (c) 2019, Intel Corporation. All rights reserved.<BR>
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+**/
+
+#ifndef _WHISKEYLAKE_ERB_ID_H_
+#define _WHISKEYLAKE_ERB_ID_H_
+
+#define BoardIdWhiskeyLakeRvp          0x60
+#endif // _WHISKEYLAKE_RVP3_ID_H_
+
