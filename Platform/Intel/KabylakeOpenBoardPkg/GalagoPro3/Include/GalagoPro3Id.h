87429bf293f6b901b42a25fb56fa92f5b9d03586
Michael Kubacki <michael.a.kubacki@intel.com>
2019-09-20 09:43:45 -0700

    KabylakeOpenBoardPkg/GalagoPro3: Add modules
    REF:https://bugzilla.tianocore.org/show_bug.cgi?id=2207
    
    Adds the modules used for System 76 Galago Pro 3 board support.
    
    This override should be removed in a future cleanup change. That is
    outside the scope of this change which is to move the contents from
    ClevoOpenBoardPkg/N1xxWU to KabylakeOpenBoardPkg/GalagoPro3.
    
    Cc: Chasel Chiu <chasel.chiu@intel.com>
    Cc: Nate DeSimone <nathaniel.l.desimone@intel.com>
    Cc: Ankit Sinha <ankit.sinha@intel.com>
    Cc: Jeremy Soller <jeremy@system76.com>
    Signed-off-by: Michael Kubacki <michael.a.kubacki@intel.com>
    Reviewed-by: Chasel Chiu <chasel.chiu@intel.com>
    Reviewed-by: Nate DeSimone <nathaniel.l.desimone@intel.com>
    Reviewed-by: Ankit Sinha <ankit.sinha@intel.com>
    Reviewed-by: Jeremy Soller <jeremy@system76.com>

--- Platform/Intel/KabylakeOpenBoardPkg/GalagoPro3/Include/GalagoPro3Id.h
+++ Platform/Intel/KabylakeOpenBoardPkg/GalagoPro3/Include/GalagoPro3Id.h
@@ -8,6 +8,6 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 #ifndef _GALAGO_PRO_3_ID_H_
 #define _GALAGO_PRO_3_ID_H_
 
-#define BoardIdGalagoPro3     0x60
+#define BoardIdGalagoPro3     0x20
 
 #endif
