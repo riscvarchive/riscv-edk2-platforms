1cd9a49eb4297fa442ca4d08341b532ba775d003
Pete Batard <pete@akeo.ie>
2019-11-19 11:38:07 +0000

    Silicon/Broadcom: Add Bcm2711 header
    Bcm2711 is the SoC used by the Raspberry Pi 4. For the most part it
    is backward compatible with the Bcm283x SoC used by older models but
    some new Bcm2711 registers have been introduced such as the ones for
    PCIe.
    
    We therefore introduce a new Bcm27xx module, which contains only the
    base address for the new registers for now (set from a PCD as is the
    case for Bcm283x) but which should get populated further as we add
    support for the Raspberry Pi 4.
    
    Signed-off-by: Pete Batard <pete@akeo.ie>
    Reviewed-by: Leif Lindholm <leif.lindholm@linaro.org>
    Reviewed-by: Philippe Mathieu-Daude <philmd@redhat.com>

---
+++ Silicon/Broadcom/Bcm27xx/Include/IndustryStandard/Bcm2711.h
@@ -0,0 +1,15 @@
+/** @file
+ *
+ *  Copyright (c) 2019, Pete Batard <pete@akeo.ie>.
+ *
+ *  SPDX-License-Identifier: BSD-2-Clause-Patent
+ *
+ **/
+
+#ifndef BCM2711_H__
+#define BCM2711_H__
+
+#define BCM2711_SOC_REGISTERS               (FixedPcdGet64 (PcdBcm27xxRegistersAddress))
+#define BCM2711_SOC_REGISTER_LENGTH         0x02000000
+
+#endif /* BCM2711_H__ */
