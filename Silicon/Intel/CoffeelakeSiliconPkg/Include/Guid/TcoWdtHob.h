478de9bc3e0611d0a2f9406423d24d0f45754b35
Nate DeSimone <nathaniel.l.desimone@intel.com>
2019-11-21 00:03:12 -0800

    CoffeeLakeSiliconPkg: TcoWdtHob.h Cleanup
    Removes duplicate EFI_HOB_GENERIC_HEADER and EFI_HOB_GUID_TYPE
    definitions from TcoWdtHob.h
    
    Cc: Chasel Chiu <chasel.chiu@intel.com>
    Cc: Michael Kubacki <michael.a.kubacki@intel.com>
    Cc: Sai Chaganty <rangasai.v.chaganty@intel.com>
    Signed-off-by: Nate DeSimone <nathaniel.l.desimone@intel.com>
    Reviewed-by: Chasel Chiu <chasel.chiu@intel.com>
    Reviewed-by: Michael Kubacki <michael.a.kubacki@intel.com>
    Reviewed-by: Sai Chaganty <rangasai.v.chaganty@intel.com>

--- Silicon/Intel/CoffeelakeSiliconPkg/Include/Guid/TcoWdtHob.h
+++ Silicon/Intel/CoffeelakeSiliconPkg/Include/Guid/TcoWdtHob.h
@@ -7,30 +7,12 @@
 #ifndef __TCO_WDT_HOB_H__
 #define __TCO_WDT_HOB_H__
 
+#include <PiPei.h>
+
 #define TCO_WDT_HOB_GUID \
   { \
     0x3e405418, 0xd8c, 0x4f1a, { 0xb0, 0x55, 0xbe, 0xf9, 0x8, 0x41, 0x46, 0x8d } \
   }
-
-#ifndef _PEI_HOB_H_
-#ifndef __HOB__H__
-#ifndef __PI_HOB_H__
-typedef struct _EFI_HOB_GENERIC_HEADER {
-  UINT16  HobType;
-  UINT16  HobLength;
-  UINT32  Reserved;
-} EFI_HOB_GENERIC_HEADER;
-
-typedef struct _EFI_HOB_GUID_TYPE {
-  EFI_HOB_GENERIC_HEADER  Header;
-  EFI_GUID                Name;
-  //
-  // Guid specific data goes here
-  //
-} EFI_HOB_GUID_TYPE;
-#endif
-#endif
-#endif
 
 typedef struct {
   EFI_HOB_GUID_TYPE Header;
@@ -38,4 +20,3 @@ typedef struct {
 } TCO_WDT_HOB;
 
 #endif
-
