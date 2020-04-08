586b23f5ad3b762e5383bf65502eb0e772865bb3
Abner Chang <abner.chang@hpe.com>
2020-04-08 14:58:58 +0800

    ProcessorPkg/Library: Add RISC-V exception library
    Initial RISC-V Supervisor Mode trap handler.
    
    Signed-off-by: Abner Chang <abner.chang@hpe.com>
    Co-authored-by: Gilbert Chen <gilbert.chen@hpe.com>
    Reviewed-by: Leif Lindholm <leif.lindholm@linaro.org>
    
    Cc: Leif Lindholm <leif.lindholm@linaro.org>
    Cc: Gilbert Chen <gilbert.chen@hpe.com>

---
+++ Silicon/RISC-V/ProcessorPkg/Library/RiscVExceptionLib/CpuExceptionHandlerLib.h
@@ -0,0 +1,16 @@
+/**@file
+
+  RISC-V Exception Handler library definition file.
+
+  Copyright (c) 2019, Hewlett Packard Enterprise Development LP. All rights reserved.<BR>
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#ifndef RISCV_CPU_EXECPTION_HANDLER_LIB_H_
+#define RISCV_CPU_EXECPTION_HANDLER_LIB_H_
+
+extern void SupervisorModeTrap(void);
+
+#endif
