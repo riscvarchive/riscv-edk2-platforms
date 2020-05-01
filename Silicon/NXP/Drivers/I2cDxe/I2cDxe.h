0874e301e987981afde1c8fb86923a06bc6e2694
Pankaj Bansal <pankaj.bansal@nxp.com>
2020-05-01 11:19:33 +0530

    Silicon/NXP: changes to use I2clib in i2cdxe
    I2c lib contains the i2c controller functionality. this can be used
    in I2c DXE driver to communicate with i2c devices.
    
    Signed-off-by: Pankaj Bansal <pankaj.bansal@nxp.com>
    Reviewed-by: Leif Lindholm <leif@nuviainc.com>

--- Silicon/NXP/Drivers/I2cDxe/I2cDxe.h
+++ Silicon/NXP/Drivers/I2cDxe/I2cDxe.h
@@ -1,7 +1,7 @@
 /** I2cDxe.h
   Header defining the constant, base address amd function for I2C controller
 
-  Copyright 2017-2019 NXP
+  Copyright 2017-2020 NXP
 
   SPDX-License-Identifier: BSD-2-Clause-Patent
 
@@ -15,32 +15,6 @@
 
 #include <Protocol/I2cMaster.h>
 #include <Protocol/NonDiscoverableDevice.h>
-
-#define I2C_CR_IIEN               (1 << 6)
-#define I2C_CR_MSTA               (1 << 5)
-#define I2C_CR_MTX                (1 << 4)
-#define I2C_CR_TX_NO_AK           (1 << 3)
-#define I2C_CR_RSTA               (1 << 2)
-
-#define I2C_SR_ICF                (1 << 7)
-#define I2C_SR_IBB                (1 << 5)
-#define I2C_SR_IAL                (1 << 4)
-#define I2C_SR_IIF                (1 << 1)
-#define I2C_SR_RX_NO_AK           (1 << 0)
-
-#define I2C_CR_IEN                (0 << 7)
-#define I2C_CR_IDIS               (1 << 7)
-#define I2C_SR_IIF_CLEAR          (1 << 1)
-
-#define BUS_IDLE                  (0 | (I2C_SR_IBB << 8))
-#define BUS_BUSY                  (I2C_SR_IBB | (I2C_SR_IBB << 8))
-#define IIF                       (I2C_SR_IIF | (I2C_SR_IIF << 8))
-
-#define I2C_FLAG_WRITE            0x0
-
-#define I2C_STATE_RETRIES         50000
-
-#define RETRY_COUNT               3
 
 #define NXP_I2C_SIGNATURE         SIGNATURE_32 ('N', 'I', '2', 'C')
 #define NXP_I2C_FROM_THIS(a)      CR ((a), NXP_I2C_MASTER, \
@@ -62,22 +36,6 @@ typedef struct {
   NXP_I2C_DEVICE_PATH             DevicePath;
   NON_DISCOVERABLE_DEVICE         *Dev;
 } NXP_I2C_MASTER;
-
-/**
-  Record defining i2c registers
-**/
-typedef struct {
-  UINT8     I2cAdr;
-  UINT8     I2cFdr;
-  UINT8     I2cCr;
-  UINT8     I2cSr;
-  UINT8     I2cDr;
-} I2C_REGS;
-
-typedef struct {
-  UINT16   SCLDivider;
-  UINT16   BusClockRate;
-} CLK_DIV;
 
 extern
 UINT64
