1f32b10a0425cd549091efd3fce9c204c189a2e8
Pankaj Bansal <pankaj.bansal@nxp.com>
2020-05-01 11:19:39 +0530

    Silicon/NXP: remove not needed components
    The structures elements and functions that are not necessary for booting
    for now are being deleted.
    Once the directory structure has been changed (i.e. we have clear
    distinction between chassis code and header files and SOC code and header
    files), we will put back the code and
    structure components back at their appropriate respective place.
    
    Also right now all the elements are being defined in structures, which are
    not being used right now. So to simplify the code restructuring, I have
    removed those for now. When we need to use those elements, we can define
    those one by one.
    
    Signed-off-by: Pankaj Bansal <pankaj.bansal@nxp.com>
    Reviewed-by: Leif Lindholm <leif@nuviainc.com>

--- Silicon/NXP/Library/SocLib/Chassis.c
+++ Silicon/NXP/Library/SocLib/Chassis.c
@@ -25,16 +25,6 @@
 #include <DramInfo.h>
 #include "NxpChassis.h"
 
-/*
- *  Structure to list available SOCs.
- *  Name, Soc Version, Number of Cores
- */
-STATIC CPU_TYPE mCpuTypeList[] = {
-  CPU_TYPE_ENTRY (LS1043A, LS1043A, 4),
-  CPU_TYPE_ENTRY (LS1046A, LS1046A, 4),
-  CPU_TYPE_ENTRY (LS2088A, LS2088A, 8),
-};
-
 UINT32
 EFIAPI
 GurRead (
@@ -46,162 +36,6 @@ GurRead (
   } else {
     return MmioRead32 (Address);
   }
-}
-
-/*
- * Return the type of initiator (core or hardware accelerator)
- */
-UINT32
-InitiatorType (
-  IN UINT32 Cluster,
-  IN UINTN  InitId
-  )
-{
-  CCSR_GUR *GurBase;
-  UINT32   Idx;
-  UINT32   Type;
-
-  GurBase = (VOID *)PcdGet64 (PcdGutsBaseAddr);
-  Idx = (Cluster >> (InitId * 8)) & TP_CLUSTER_INIT_MASK;
-  Type = GurRead ((UINTN)&GurBase->TpItyp[Idx]);
-
-  if (Type & TP_ITYP_AV_MASK) {
-    return Type;
-  }
-
-  return 0;
-}
-
-/*
- *  Return the mask for number of cores on this SOC.
- */
-UINT32
-CpuMask (
-  VOID
-  )
-{
-  CCSR_GUR  *GurBase;
-  UINTN     ClusterIndex;
-  UINTN     Count;
-  UINT32    Cluster;
-  UINT32    Type;
-  UINT32    Mask;
-  UINTN     InitiatorIndex;
-
-  GurBase = (VOID *)PcdGet64 (PcdGutsBaseAddr);
-  ClusterIndex = 0;
-  Count = 0;
-  Mask = 0;
-
-  do {
-    Cluster = GurRead ((UINTN)&GurBase->TpCluster[ClusterIndex].Lower);
-    for (InitiatorIndex = 0; InitiatorIndex < TP_INIT_PER_CLUSTER; InitiatorIndex++) {
-      Type = InitiatorType (Cluster, InitiatorIndex);
-      if (Type) {
-        if (TP_ITYP_TYPE_MASK (Type) == TP_ITYP_TYPE_ARM) {
-          Mask |= 1 << Count;
-        }
-        Count++;
-      }
-    }
-    ClusterIndex++;
-  } while (CHECK_CLUSTER (Cluster));
-
-  return Mask;
-}
-
-/*
- *  Return the number of cores on this SOC.
- */
-UINTN
-CpuNumCores (
-  VOID
-  )
-{
-  UINTN Count;
-  UINTN Num;
-
-  Count = 0;
-  Num = CpuMask ();
-
-  while (Num) {
-    Count += Num & 1;
-    Num >>= 1;
-  }
-
-  return Count;
-}
-
-/*
- *  Return core's cluster
- */
-INT32
-QoriqCoreToCluster (
-  IN UINTN Core
-  )
-{
-  CCSR_GUR  *GurBase;
-  UINTN     ClusterIndex;
-  UINTN     Count;
-  UINT32    Cluster;
-  UINT32    Type;
-  UINTN     InitiatorIndex;
-
-  GurBase = (VOID *)PcdGet64 (PcdGutsBaseAddr);
-  ClusterIndex = 0;
-  Count = 0;
-  do {
-    Cluster = GurRead ((UINTN)&GurBase->TpCluster[ClusterIndex].Lower);
-    for (InitiatorIndex = 0; InitiatorIndex < TP_INIT_PER_CLUSTER; InitiatorIndex++) {
-      Type = InitiatorType (Cluster, InitiatorIndex);
-      if (Type) {
-        if (Count == Core) {
-          return ClusterIndex;
-        }
-        Count++;
-      }
-    }
-    ClusterIndex++;
-  } while (CHECK_CLUSTER (Cluster));
-
-  return -1;      // cannot identify the cluster
-}
-
-/*
- *  Return the type of core i.e. A53, A57 etc of inputted
- *  core number.
- */
-UINTN
-QoriqCoreToType (
-  IN UINTN Core
-  )
-{
-  CCSR_GUR  *GurBase;
-  UINTN     ClusterIndex;
-  UINTN     Count;
-  UINT32    Cluster;
-  UINT32    Type;
-  UINTN     InitiatorIndex;
-
-  GurBase = (VOID *)PcdGet64 (PcdGutsBaseAddr);
-  ClusterIndex = 0;
-  Count = 0;
-
-  do {
-    Cluster = GurRead ((UINTN)&GurBase->TpCluster[ClusterIndex].Lower);
-    for (InitiatorIndex = 0; InitiatorIndex < TP_INIT_PER_CLUSTER; InitiatorIndex++) {
-      Type = InitiatorType (Cluster, InitiatorIndex);
-      if (Type) {
-        if (Count == Core) {
-          return Type;
-        }
-        Count++;
-      }
-    }
-    ClusterIndex++;
-  } while (CHECK_CLUSTER (Cluster));
-
-  return EFI_NOT_FOUND;      /* cannot identify the cluster */
 }
 
 /*
@@ -217,21 +51,6 @@ GetBusFrequency (
   GetSysInfo (&SocSysInfo);
 
   return SocSysInfo.FreqSystemBus;
-}
-
-/*
- * Return SDXC bus frequency
- */
-UINT64
-GetSdxcFrequency (
-   VOID
-  )
-{
-  SYS_INFO SocSysInfo;
-
-  GetSysInfo (&SocSysInfo);
-
-  return SocSysInfo.FreqSdhc;
 }
 
 /*
@@ -254,33 +73,6 @@ SmmuInit (
 
   Value = (MmioRead32 ((UINTN)SMMU_REG_NSCR0) | SCR0_CLIENTPD_MASK) & ~SCR0_USFCFG_MASK;
   MmioWrite32 ((UINTN)SMMU_REG_NSCR0, Value);
-}
-
-/*
- * Return current Soc Name form mCpuTypeList
- */
-CHAR8 *
-GetSocName (
-  VOID
-  )
-{
-  UINT8     Count;
-  UINTN     Svr;
-  UINTN     Ver;
-  CCSR_GUR  *GurBase;
-
-  GurBase = (VOID *)PcdGet64 (PcdGutsBaseAddr);
-
-  Svr = GurRead ((UINTN)&GurBase->Svr);
-  Ver = SVR_SOC_VER (Svr);
-
-  for (Count = 0; Count < ARRAY_SIZE (mCpuTypeList); Count++) {
-    if ((mCpuTypeList[Count].SocVer & SVR_WO_E) == Ver) {
-      return (CHAR8 *)mCpuTypeList[Count].Name;
-    }
-  }
-
-  return NULL;
 }
 
 UINTN
--- Silicon/NXP/Library/SocLib/Chassis2/Soc.c
+++ Silicon/NXP/Library/SocLib/Chassis2/Soc.c
@@ -32,38 +32,14 @@ GetSysInfo (
   )
 {
   CCSR_GUR     *GurBase;
-  CCSR_CLOCK   *ClkBase;
-  UINTN        CpuIndex;
-  UINT32       TempRcw;
-  UINT32       CPllSel;
-  UINT32       CplxPll;
-  CONST UINT8  CoreCplxPll[8] = {
-    [0] = 0,    /* CC1 PPL / 1 */
-    [1] = 0,    /* CC1 PPL / 2 */
-    [4] = 1,    /* CC2 PPL / 1 */
-    [5] = 1,    /* CC2 PPL / 2 */
-  };
-
-  CONST UINT8  CoreCplxPllDivisor[8] = {
-    [0] = 1,    /* CC1 PPL / 1 */
-    [1] = 2,    /* CC1 PPL / 2 */
-    [4] = 1,    /* CC2 PPL / 1 */
-    [5] = 2,    /* CC2 PPL / 2 */
-  };
-
-  UINTN        PllCount;
-  UINTN        FreqCPll[NUM_CC_PLLS];
-  UINTN        PllRatio[NUM_CC_PLLS];
   UINTN        SysClk;
 
   GurBase = (VOID *)PcdGet64 (PcdGutsBaseAddr);
-  ClkBase = (VOID *)PcdGet64 (PcdClkBaseAddr);
   SysClk = CLK_FREQ;
 
   SetMem (PtrSysInfo, sizeof (SYS_INFO), 0);
 
   PtrSysInfo->FreqSystemBus = SysClk;
-  PtrSysInfo->FreqDdrBus = SysClk;
 
   //
   // selects the platform clock:SYSCLK ratio and calculate
@@ -72,61 +48,6 @@ GetSysInfo (
   PtrSysInfo->FreqSystemBus *= (GurRead ((UINTN)&GurBase->RcwSr[0]) >>
                 CHASSIS2_RCWSR0_SYS_PLL_RAT_SHIFT) &
                 CHASSIS2_RCWSR0_SYS_PLL_RAT_MASK;
-  //
-  // selects the DDR PLL:SYSCLK Ratio and calculate DDR frequency
-  //
-  PtrSysInfo->FreqDdrBus *= (GurRead ((UINTN)&GurBase->RcwSr[0]) >>
-                CHASSIS2_RCWSR0_MEM_PLL_RAT_SHIFT) &
-                CHASSIS2_RCWSR0_MEM_PLL_RAT_MASK;
-
-  for (PllCount = 0; PllCount < NUM_CC_PLLS; PllCount++) {
-    PllRatio[PllCount] = (GurRead ((UINTN)&ClkBase->PllCgSr[PllCount].PllCnGSr) >> 1) & 0xff;
-    if (PllRatio[PllCount] > 4) {
-      FreqCPll[PllCount] = SysClk * PllRatio[PllCount];
-    } else {
-      FreqCPll[PllCount] = PtrSysInfo->FreqSystemBus * PllRatio[PllCount];
-    }
-  }
-
-  //
-  // Calculate Core frequency
-  //
-  for (CpuIndex = 0; CpuIndex < MAX_CPUS; CpuIndex++) {
-    CPllSel = (GurRead ((UINTN)&ClkBase->ClkcSr[CpuIndex].ClkCnCSr) >> 27) & 0xf;
-    CplxPll = CoreCplxPll[CPllSel];
-
-    PtrSysInfo->FreqProcessor[CpuIndex] = FreqCPll[CplxPll] / CoreCplxPllDivisor[CPllSel];
-  }
-
-  //
-  // Calculate FMAN frequency
-  //
-  TempRcw = GurRead ((UINTN)&GurBase->RcwSr[7]);
-  switch ((TempRcw & HWA_CGA_M1_CLK_SEL) >> HWA_CGA_M1_CLK_SHIFT) {
-  case 2:
-    PtrSysInfo->FreqFman[0] = FreqCPll[0] / 2;
-    break;
-  case 3:
-    PtrSysInfo->FreqFman[0] = FreqCPll[0] / 3;
-    break;
-  case 4:
-    PtrSysInfo->FreqFman[0] = FreqCPll[0] / 4;
-    break;
-  case 5:
-    PtrSysInfo->FreqFman[0] = PtrSysInfo->FreqSystemBus;
-    break;
-  case 6:
-    PtrSysInfo->FreqFman[0] = FreqCPll[1] / 2;
-    break;
-  case 7:
-    PtrSysInfo->FreqFman[0] = FreqCPll[1] / 3;
-    break;
-  default:
-    DEBUG ((DEBUG_WARN, "Error: Unknown FMan1 clock select!\n"));
-    break;
-  }
-  PtrSysInfo->FreqSdhc = PtrSysInfo->FreqSystemBus/PcdGet32 (PcdPlatformFreqDiv);
-  PtrSysInfo->FreqQman = PtrSysInfo->FreqSystemBus/PcdGet32 (PcdPlatformFreqDiv);
 }
 
 /**
