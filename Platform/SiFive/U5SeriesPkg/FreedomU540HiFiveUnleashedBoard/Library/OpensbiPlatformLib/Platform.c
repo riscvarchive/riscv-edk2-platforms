705ba2c01adb618920bf308eef51d800027850e6
Abner Chang <abner.chang@hpe.com>
2020-07-06 15:53:53 +0800

    U540 platform
    Bump up to OpenSBI v0.8, this will be rebased once it is reviewed.
    
    Signed-off-by: Abner Chang <abner.chang@hpe.com>

--- Platform/SiFive/U5SeriesPkg/FreedomU540HiFiveUnleashedBoard/Library/OpensbiPlatformLib/Platform.c
+++ Platform/SiFive/U5SeriesPkg/FreedomU540HiFiveUnleashedBoard/Library/OpensbiPlatformLib/Platform.c
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright (c) 2019, Hewlett Packard Enterprise Development LP. All rights reserved.<BR>
+ * Copyright (c) 2020, Hewlett Packard Enterprise Development LP. All rights reserved.<BR>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  *
@@ -11,13 +11,13 @@
  */
 
 #include <libfdt.h>
-#include <fdt.h>
-#include <sbi/riscv_encoding.h>
-#include <sbi/sbi_const.h>
-#include <sbi/sbi_hart.h>
-#include <sbi/sbi_console.h>
-#include <sbi/sbi_platform.h>
+#include <sbi/riscv_asm.h>
 #include <sbi/riscv_io.h>
+#include <sbi/riscv_encoding.h>
+#include <sbi/sbi_console.h>
+#include <sbi/sbi_const.h>
+#include <sbi/sbi_platform.h>
+#include <sbi_utils/fdt/fdt_fixup.h>
 #include <sbi_utils/irqchip/plic.h>
 #include <sbi_utils/serial/sifive-uart.h>
 #include <sbi_utils/sys/clint.h>
@@ -37,23 +37,28 @@
 
 #define U540_UART_BAUDRATE        115200
 
-/**
- * The U540 SoC has 5 HARTs but HART ID 0 has only SMode.
- * HARTs 1 is selected as boot HART
- */
-#ifndef U540_ENABLED_HART_MASK
-#define U540_ENABLED_HART_MASK  (1 << U540_BOOT_HART_ID)
-#endif
-
-#define U540_HARTID_DISABLED    ~(U540_ENABLED_HART_MASK)
-
 /* PRCI clock related macros */
 //TODO: Do we need a separate driver for this ?
 #define U540_PRCI_BASE_ADDR                 0x10000000
 #define U540_PRCI_CLKMUXSTATUSREG           0x002C
 #define U540_PRCI_CLKMUX_STATUS_TLCLKSEL    (0x1 << 1)
 
+/* Full tlb flush always */
+#define U540_TLB_RANGE_FLUSH_LIMIT 0
+
 unsigned long log2roundup(unsigned long x);
+
+static struct plic_data plic = {
+    .addr = U540_PLIC_ADDR,
+    .num_src = U540_PLIC_NUM_SOURCES,
+};
+
+static struct clint_data clint = {
+    .addr = CLINT_REG_BASE_ADDR,
+    .first_hartid = 0,
+    .hart_count = U540_HART_COUNT,
+    .has_64bit_mmio = TRUE,
+};
 
 static void U540_modify_dt(void *fdt)
 {
@@ -83,7 +88,7 @@ static void U540_modify_dt(void *fdt)
     fdt_setprop_string(fdt, chosen_offset, "stdout-path",
                "/soc/serial@10010000:115200");
 
-    plic_fdt_fixup(fdt, "riscv,plic0");
+    fdt_plic_fixup(fdt, "riscv,plic0");
 }
 
 static int U540_final_init(bool cold_boot)
@@ -140,17 +145,15 @@ static int U540_console_init(void)
 static int U540_irqchip_init(bool cold_boot)
 {
     int rc;
-    u32 hartid = sbi_current_hartid();
+    u32 hartid = current_hartid();
 
     if (cold_boot) {
-        rc = plic_cold_irqchip_init(U540_PLIC_ADDR,
-                        U540_PLIC_NUM_SOURCES,
-                        U540_HART_COUNT);
+        rc = plic_cold_irqchip_init(&plic);
         if (rc)
             return rc;
     }
 
-    return plic_warm_irqchip_init(hartid,
+    return plic_warm_irqchip_init(&plic,
             (hartid) ? (2 * hartid - 1) : 0,
             (hartid) ? (2 * hartid) : -1);
 }
@@ -160,8 +163,7 @@ static int U540_ipi_init(bool cold_boot)
     int rc;
 
     if (cold_boot) {
-        rc = clint_cold_ipi_init(CLINT_REG_BASE_ADDR,
-                     U540_HART_COUNT);
+        rc = clint_cold_ipi_init(&clint);
         if (rc)
             return rc;
 
@@ -170,21 +172,29 @@ static int U540_ipi_init(bool cold_boot)
     return clint_warm_ipi_init();
 }
 
+static u64 U540_get_tlbr_flush_limit(void)
+{
+    return U540_TLB_RANGE_FLUSH_LIMIT;
+}
+
 static int U540_timer_init(bool cold_boot)
 {
     int rc;
 
     if (cold_boot) {
-        rc = clint_cold_timer_init(CLINT_REG_BASE_ADDR,
-                       U540_HART_COUNT, TRUE);
+        rc = clint_cold_timer_init(&clint, NULL);
         if (rc)
             return rc;
     }
 
     return clint_warm_timer_init();
 }
+/**
+ * The U540 SoC has 5 HARTs, HART 1 s the boot hart.
+ */
+static u32 U540_hart_index2id[U540_HART_COUNT] = {0, 1, 2, 3, 4};
 
-static int U540_system_down(u32 type)
+static int U540_system_reset(u32 type)
 {
     /* For now nothing to do. */
     return 0;
@@ -201,21 +211,21 @@ const struct sbi_platform_operations platform_ops = {
     .ipi_send = clint_ipi_send,
     .ipi_clear = clint_ipi_clear,
     .ipi_init = U540_ipi_init,
+    .get_tlbr_flush_limit = U540_get_tlbr_flush_limit,
     .timer_value = clint_timer_value,
     .timer_event_stop = clint_timer_event_stop,
     .timer_event_start = clint_timer_event_start,
     .timer_init = U540_timer_init,
-    .system_reboot = U540_system_down,
-    .system_shutdown = U540_system_down
+    .system_reset = U540_system_reset
 };
 
 const struct sbi_platform platform = {
     .opensbi_version    = OPENSBI_VERSION,                      // The OpenSBI version this platform table is built bassed on.
     .platform_version   = SBI_PLATFORM_VERSION(0x0001, 0x0000), // SBI Platform version 1.0
-    .name                   = "SiFive Freedom U540",
-    .features       = SBI_PLATFORM_DEFAULT_FEATURES,
-    .hart_count     = U540_HART_COUNT,
+    .name               = "SiFive Freedom U540",
+    .features           = SBI_PLATFORM_DEFAULT_FEATURES,
+    .hart_count         = U540_HART_COUNT,
+    .hart_index2id      = U540_hart_index2id,
     .hart_stack_size    = U540_HART_STACK_SIZE,
-    .disabled_hart_mask = U540_HARTID_DISABLED,
     .platform_ops_addr  = (unsigned long)&platform_ops
 };
