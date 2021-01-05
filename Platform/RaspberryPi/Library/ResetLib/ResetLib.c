riscvarchive/riscv-edk2-platforms

commit 94e9fba43d7e132be3c582c676968a7f408072c1

Platform/RaspberryPi: Add system/user defined reset delay
Due to the method in which NV variables are stored on removable media
for the Raspberry Pi platform, and the manner in which we dump updated
variables right before reset, it is possible, and has been repeatedly
demonstrated with SSD-based USB 3.0 devices, that the updated file does
not actually end up being written to permanent storage, due to the
device write-cache not having enough time to be flushed before reset.

To compensate for this, since we don't know of a generic method that
would allow turning off USB mass storage devices write cache (and also
because we are seeing an issue that seems related for SD-based media),
we add a new reset delay PCD, which can be set by the user, and which
we also set as required when NV variables are being dumped.

Our testing show that, with more than 3 seconds of extra delay, the
storage media has enough time to finalize its internal write, thus
solving the issue of configuration changes not being persisted.

Signed-off-by: Pete Batard <pete@akeo.ie>
Reviewed-by: Andrei Warkentin <awarkentin@vmware.com>
Tested-by: Samer El-Haj-Mahmoud <Samer.El-Haj-Mahmoud@arm.com>

Platform/RaspberryPi/Library/ResetLib/ResetLib.c
@@ -16,6 +16,7 @@
 
 #include <Library/BaseLib.h>
 #include <Library/DebugLib.h>
+#include <Library/TimerLib.h>
 #include <Library/EfiResetSystemLib.h>
 #include <Library/ArmSmcLib.h>
 #include <Library/UefiLib.h>
@@ -44,6 +45,7 @@ LibResetSystem (
   )
 {
   ARM_SMC_ARGS ArmSmcArgs;
+  UINT32 Delay;
 
   if (!EfiAtRuntime ()) {
     /*
@@ -51,6 +53,15 @@ LibResetSystem (
      */
     EfiEventGroupSignal (&gRaspberryPiEventResetGuid);
   }
+
+  Delay = PcdGet32 (PcdPlatformResetDelay);
+  if (Delay != 0) {
+    DEBUG ((DEBUG_INFO, "Platform will be reset in %d.%d seconds...\n",
+            Delay / 1000000, (Delay % 1000000) / 100000));
+    MicroSecondDelay (Delay);
+  }
+  DEBUG ((DEBUG_INFO, "Platform %a.\n",
+          (ResetType == EfiResetShutdown) ? "shutdown" : "reset"));
 
   switch (ResetType) {
   case EfiResetPlatformSpecific:

Platform/RaspberryPi/Drivers/VarBlockServiceDxe/VarBlockServiceDxe.c
@@ -10,6 +10,18 @@
 
 #include "VarBlockService.h"
 
+//
+// Minimum delay to enact before reset, when variables are dirty (in μs).
+// Needed to ensure that SSD-based USB 3.0 devices have time to flush their
+// write cache after updating the NV vars. A much smaller delay is applied
+// on Pi 3 compared to Pi 4, as we haven't had reports of issues there yet.
+//
+#if (RPI_MODEL == 3)
+#define PLATFORM_RESET_DELAY     500000
+#else
+#define PLATFORM_RESET_DELAY    3500000
+#endif
+
 VOID *mSFSRegistration;
 
 
@@ -154,6 +166,7 @@ DumpVars (
   )
 {
   EFI_STATUS Status;
+  RETURN_STATUS PcdStatus;
 
   if (mFvInstance->Device == NULL) {
     DEBUG ((DEBUG_INFO, "Variable store not found?\n"));
@@ -173,6 +186,17 @@ DumpVars (
   }
 
   DEBUG ((DEBUG_INFO, "Variables dumped!\n"));
+
+  //
+  // Add a reset delay to give time for slow/cached devices
+  // to flush the NV variables write to permanent storage.
+  // But only do so if this won't reduce an existing user-set delay.
+  //
+  if (PcdGet32 (PcdPlatformResetDelay) < PLATFORM_RESET_DELAY) {
+    PcdStatus = PcdSet32S (PcdPlatformResetDelay, PLATFORM_RESET_DELAY);
+    ASSERT_RETURN_ERROR (PcdStatus);
+  }
+
   mFvInstance->Dirty = FALSE;
 }
 

Platform/RaspberryPi/Drivers/VarBlockServiceDxe/VarBlockServiceDxe.inf
@@ -79,6 +79,7 @@
   gEfiMdeModulePkgTokenSpaceGuid.PcdFlashNvStorageFtwSpareBase
   gEfiMdeModulePkgTokenSpaceGuid.PcdFlashNvStorageVariableBase
   gRaspberryPiTokenSpaceGuid.PcdNvStorageEventLogBase
+  gRaspberryPiTokenSpaceGuid.PcdPlatformResetDelay
   gEfiMdeModulePkgTokenSpaceGuid.PcdFlashNvStorageVariableBase64
 
 [FeaturePcd]

Platform/RaspberryPi/Library/ResetLib/ResetLib.inf
@@ -33,8 +33,13 @@
   DebugLib
   BaseLib
   ArmSmcLib
+  PcdLib
+  TimerLib
   UefiLib
   UefiRuntimeLib
 
 [Guids]
   gRaspberryPiEventResetGuid
+
+[Pcd]
+  gRaspberryPiTokenSpaceGuid.PcdPlatformResetDelay      ## CONSUMES

Platform/RaspberryPi/RPi3/RPi3.dsc
@@ -509,6 +509,12 @@
   gRaspberryPiTokenSpaceGuid.PcdFanTemp|L"FanTemp"|gConfigDxeFormSetGuid|0x0|0
 
   #
+  # Reset-related.
+  #
+
+  gRaspberryPiTokenSpaceGuid.PcdPlatformResetDelay|L"ResetDelay"|gRaspberryPiTokenSpaceGuid|0x0|0
+
+  #
   # Common UEFI ones.
   #
 

Platform/RaspberryPi/RPi4/RPi4.dsc
@@ -523,6 +523,12 @@
   gRaspberryPiTokenSpaceGuid.PcdFanTemp|L"FanTemp"|gConfigDxeFormSetGuid|0x0|60
 
   #
+  # Reset-related.
+  #
+
+  gRaspberryPiTokenSpaceGuid.PcdPlatformResetDelay|L"ResetDelay"|gRaspberryPiTokenSpaceGuid|0x0|0
+
+  #
   # Common UEFI ones.
   #
 

Platform/RaspberryPi/RaspberryPi.dec
@@ -68,3 +68,4 @@
   gRaspberryPiTokenSpaceGuid.PcdRamLimitTo3GB|0|UINT32|0x0000001A
   gRaspberryPiTokenSpaceGuid.PcdFanOnGpio|0|UINT32|0x0000001C
   gRaspberryPiTokenSpaceGuid.PcdFanTemp|0|UINT32|0x0000001D
+  gRaspberryPiTokenSpaceGuid.PcdPlatformResetDelay|0|UINT32|0x0000001E

commit ae6c236e7610ecb0459b30ca22e2fe88d7a5c1b2

Platform/RaspberryPi: Fix Linux kernel panic on reset/poweroff
Commit 94e9fba43d7e132be3c582c676968a7f408072c1 introduced an unconditional
call to PcdGet32 after we exit boot services, that produces a kernel panic
on Linux reset.

This addendum to the previous commit ensures that we only read the PCD and
apply the delay while we are still in UEFI, which is what we want anyway as
the goal was to fix the storage of NV variables set by the user from within
the UEFI firmware interface.

Signed-off-by: Pete Batard <pete@akeo.ie>
Reviewed-by: Ard Biesheuvel <ard.biesheuvel@arm.com>

Platform/RaspberryPi/Library/ResetLib/ResetLib.c
@@ -52,13 +52,13 @@ LibResetSystem (
      * Only if still in UEFI.
      */
     EfiEventGroupSignal (&gRaspberryPiEventResetGuid);
-  }
 
-  Delay = PcdGet32 (PcdPlatformResetDelay);
-  if (Delay != 0) {
-    DEBUG ((DEBUG_INFO, "Platform will be reset in %d.%d seconds...\n",
-            Delay / 1000000, (Delay % 1000000) / 100000));
-    MicroSecondDelay (Delay);
+    Delay = PcdGet32 (PcdPlatformResetDelay);
+    if (Delay != 0) {
+      DEBUG ((DEBUG_INFO, "Platform will be reset in %d.%d seconds...\n",
+              Delay / 1000000, (Delay % 1000000) / 100000));
+      MicroSecondDelay (Delay);
+    }
   }
   DEBUG ((DEBUG_INFO, "Platform %a.\n",
           (ResetType == EfiResetShutdown) ? "shutdown" : "reset"));