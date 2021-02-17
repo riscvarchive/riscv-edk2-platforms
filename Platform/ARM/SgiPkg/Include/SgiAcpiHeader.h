912c391b6ed31a33aef5cda96f3442f087d17727
Vijayenthiran Subramaniam via groups.io <vijayenthiran.subramaniam=arm.com@groups.io>
2021-02-17 16:31:55 +0530

    Platform/ARM/SgiPkg: Add helper macros for HMAT table
    Add helper macros to create Memory Proximity Domain, System Locality
    Latency and Bandwidth Information, Memory Side Cache Information
    structures in the HMAT table.
    
    Signed-off-by: Vijayenthiran Subramaniam <vijayenthiran.subramaniam@arm.com>
    Reviewed-by: Sami Mujawar <sami.mujawar@arm.com>

--- Platform/ARM/SgiPkg/Include/SgiAcpiHeader.h
+++ Platform/ARM/SgiPkg/Include/SgiAcpiHeader.h
@@ -1,6 +1,6 @@
 /** @file
 *
-*  Copyright (c) 2018-2020, ARM Limited. All rights reserved.
+*  Copyright (c) 2018-2021, ARM Limited. All rights reserved.
 *
 *  SPDX-License-Identifier: BSD-2-Clause-Patent
 *
@@ -118,5 +118,132 @@
     3, sizeof (EFI_ACPI_6_3_GICC_AFFINITY_STRUCTURE), ProximityDomain,         \
     ACPIProcessorUID,  Flags,  ClockDomain                                     \
   }
+
+//
+// HMAT related structures
+//
+// Memory Proximity Domain Attributes Structure
+// Refer Section 5.2.27.3 in ACPI Specification, Version 6.3
+#define EFI_ACPI_6_3_HMAT_STRUCTURE_MEMORY_PROXIMITY_DOMAIN_ATTRIBUTES_INIT(   \
+    Flags, ProximityDomainForAttachedIntiator, ProximityDomainForMemory)       \
+  {                                                                            \
+    EFI_ACPI_6_3_HMAT_TYPE_MEMORY_PROXIMITY_DOMAIN_ATTRIBUTES,                 \
+    {                                                                          \
+      EFI_ACPI_RESERVED_BYTE,                                                  \
+      EFI_ACPI_RESERVED_BYTE                                                   \
+    },                                                                         \
+    sizeof (EFI_ACPI_6_3_HMAT_STRUCTURE_MEMORY_PROXIMITY_DOMAIN_ATTRIBUTES),   \
+    {                                                                          \
+      Flags,                                                                   \
+      0                                                                        \
+    },                                                                         \
+    {                                                                          \
+      EFI_ACPI_RESERVED_BYTE,                                                  \
+      EFI_ACPI_RESERVED_BYTE                                                   \
+    },                                                                         \
+    ProximityDomainForAttachedIntiator,                                        \
+    ProximityDomainForMemory,                                                  \
+    {                                                                          \
+      EFI_ACPI_RESERVED_BYTE,                                                  \
+      EFI_ACPI_RESERVED_BYTE,                                                  \
+      EFI_ACPI_RESERVED_BYTE,                                                  \
+      EFI_ACPI_RESERVED_BYTE,                                                  \
+      EFI_ACPI_RESERVED_BYTE,                                                  \
+      EFI_ACPI_RESERVED_BYTE,                                                  \
+      EFI_ACPI_RESERVED_BYTE,                                                  \
+      EFI_ACPI_RESERVED_BYTE,                                                  \
+      EFI_ACPI_RESERVED_BYTE,                                                  \
+      EFI_ACPI_RESERVED_BYTE,                                                  \
+      EFI_ACPI_RESERVED_BYTE,                                                  \
+      EFI_ACPI_RESERVED_BYTE,                                                  \
+      EFI_ACPI_RESERVED_BYTE,                                                  \
+      EFI_ACPI_RESERVED_BYTE,                                                  \
+      EFI_ACPI_RESERVED_BYTE,                                                  \
+      EFI_ACPI_RESERVED_BYTE,                                                  \
+      EFI_ACPI_RESERVED_BYTE,                                                  \
+      EFI_ACPI_RESERVED_BYTE,                                                  \
+      EFI_ACPI_RESERVED_BYTE,                                                  \
+      EFI_ACPI_RESERVED_BYTE                                                   \
+    }                                                                          \
+  }
+
+// System Locality Latency and Bandwidth Information Structure
+// Refer Section 5.2.27.4 in ACPI Specification, Version 6.3
+#define EFI_ACPI_6_3_HMAT_STRUCTURE_SYSTEM_LOCALITY_LATENCY_AND_BANDWIDTH_INFO_INIT(  \
+    Flags, DataType, NumInitiatorProximityDomains,                                    \
+    NumTargetProximityDomains, EntryBaseUnit)                                         \
+  {                                                                                   \
+    EFI_ACPI_6_3_HMAT_TYPE_SYSTEM_LOCALITY_LATENCY_AND_BANDWIDTH_INFO,                \
+    {                                                                                 \
+      EFI_ACPI_RESERVED_BYTE,                                                         \
+      EFI_ACPI_RESERVED_BYTE                                                          \
+    },                                                                                \
+    sizeof (EFI_ACPI_6_3_HMAT_STRUCTURE_SYSTEM_LOCALITY_LATENCY_AND_BANDWIDTH_INFO) + \
+      (4 * NumInitiatorProximityDomains) + (4 * NumTargetProximityDomains) +          \
+      (2 * NumInitiatorProximityDomains * NumTargetProximityDomains),                 \
+    {                                                                                 \
+      Flags,                                                                          \
+      0                                                                               \
+    },                                                                                \
+    DataType,                                                                         \
+    {                                                                                 \
+      EFI_ACPI_RESERVED_BYTE,                                                         \
+      EFI_ACPI_RESERVED_BYTE                                                          \
+    },                                                                                \
+    NumInitiatorProximityDomains,                                                     \
+    NumTargetProximityDomains,                                                        \
+    {                                                                                 \
+      EFI_ACPI_RESERVED_BYTE,                                                         \
+      EFI_ACPI_RESERVED_BYTE,                                                         \
+      EFI_ACPI_RESERVED_BYTE,                                                         \
+      EFI_ACPI_RESERVED_BYTE                                                          \
+    },                                                                                \
+    EntryBaseUnit                                                                     \
+  }
+
+// Memory Side Cache Information Structure
+// Refer Section 5.2.27.5 in ACPI Specification, Version 6.3
+#define EFI_ACPI_6_3_HMAT_STRUCTURE_MEMORY_SIDE_CACHE_INFO_INIT(               \
+    MemoryProximityDomain, MemorySideCacheSize, CacheAttributes,               \
+    NumberOfSmbiosHandles)                                                     \
+  {                                                                            \
+    EFI_ACPI_6_3_HMAT_TYPE_MEMORY_SIDE_CACHE_INFO,                             \
+    {                                                                          \
+      EFI_ACPI_RESERVED_BYTE,                                                  \
+      EFI_ACPI_RESERVED_BYTE                                                   \
+    },                                                                         \
+    sizeof (EFI_ACPI_6_3_HMAT_STRUCTURE_MEMORY_SIDE_CACHE_INFO) +              \
+      (NumberOfSmbiosHandles * 2),                                             \
+    MemoryProximityDomain,                                                     \
+    {                                                                          \
+      EFI_ACPI_RESERVED_BYTE,                                                  \
+      EFI_ACPI_RESERVED_BYTE,                                                  \
+      EFI_ACPI_RESERVED_BYTE,                                                  \
+      EFI_ACPI_RESERVED_BYTE                                                   \
+    },                                                                         \
+    MemorySideCacheSize,                                                       \
+    CacheAttributes,                                                           \
+    {                                                                          \
+      EFI_ACPI_RESERVED_BYTE,                                                  \
+      EFI_ACPI_RESERVED_BYTE                                                   \
+    },                                                                         \
+    NumberOfSmbiosHandles                                                      \
+  }
+
+/** A macro to initialise the Memory Side Cache Information Attributes.
+    See Table 5.124 in ACPI Specification, Version 6.3
+
+  @param [in] TotalCacheLevels    Total Cache Levels for this Memory Proximity.
+  @param [in] CacheLevel          Cache Level described in this structure.
+  @param [in] CacheAssociativity  Cache Associativity.
+  @param [in] WritePolicy         Write Policy.
+  @param [in] CacheLineSize       Cache Line size in bytes.
+**/
+#define HMAT_STRUCTURE_MEMORY_SIDE_CACHE_INFO_CACHE_ATTRIBUTES_INIT(           \
+  TotalCacheLevels, CacheLevel, CacheAssociativity, WritePolicy, CacheLineSize \
+  )                                                                            \
+{                                                                              \
+  TotalCacheLevels, CacheLevel, CacheAssociativity, WritePolicy, CacheLineSize \
+}
 
 #endif /* __SGI_ACPI_HEADER__ */
