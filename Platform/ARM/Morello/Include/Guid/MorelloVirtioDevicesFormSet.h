a630b97938e0357485789a9d3d75f90c67e82aaa
Anurag Koul <anurag.koul@arm.com>
2021-04-19 11:10:39 +0530

    Platform/ARM/Morello: Add PlatformDxe driver for Morello
    This patch adds the PlatformDxe Driver for Morello
    platform. It includes the registration of the ramdisk
    device.
    
    Co-authored-by: Chandni Cherukuri <chandni.cherukuri@arm.com>
    Signed-off-by: Chandni Cherukuri <chandni.cherukuri@arm.com>
    Reviewed-by: Sami Mujawar <sami.mujawar@arm.com>

---
+++ Platform/ARM/Morello/Include/Guid/MorelloVirtioDevicesFormSet.h
@@ -0,0 +1,14 @@
+/** @file
+
+  Copyright (c) 2021, ARM Limited. All rights reserved.<BR>
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+**/
+
+#ifndef  MORELLO_VIRTIO_DEVICES_FORMSET_H_
+#define  MORELLO_VIRTIO_DEVICES_FORMSET_H_
+
+#define MORELLO_VIRTIO_BLOCK_GUID  \
+  { 0x2B6E62D0, 0x9346, 0x4E1A, { 0xAA, 0x1E, 0xCB, 0x01, 0xC3, 0x23, 0x4A, 0x00 } }
+
+#endif //MORELLO_VIRTIO_DEVICES_FORMSET_H_
