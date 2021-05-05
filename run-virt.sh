ba1c83bdd78c3fd19ef0581956b9f1c95adc0223
Daniel Schaefer <git@danielschaefer.me>
2021-05-05 15:30:08 +0800

    one stage

--- run-virt.sh
+++ run-virt.sh
@@ -10,7 +10,7 @@ timeout --foreground 10 \
 ls -l U540.fd
 timeout --foreground 10 \
   qemu-system-riscv64 -machine virt \
-  -bios U540.fd \
+  -bios Build/FreedomU540HiFiveUnleashed/DEBUG_GCC5/FV/U540.fd \
   -m 1024 -nographic -smp cpus=1,maxcpus=1 | tee boot.log \
   || true
 
