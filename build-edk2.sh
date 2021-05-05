c3a32a333f6fc51a52896d01fd444f9361c06ae8
Daniel Schaefer <git@danielschaefer.me>
2021-05-05 13:34:03 +0800

    Build U500, too

--- build-edk2.sh
+++ build-edk2.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-ls -l
-ls -l edk2
-ls -l edk2-platforms
 export GCC5_RISCV64_PREFIX=/riscv-gnu-toolchain-binaries/bin/riscv64-unknown-elf-
 export PACKAGES_PATH=/edk2:/edk2-platforms
 export EDK_TOOLS_PATH=/edk2/BaseTools
@@ -14,9 +11,9 @@ make -C edk2/BaseTools/Source/C
 source edk2/edksetup.sh BaseTools
 
 cd /edk2-platforms
-build -a RISCV64 -p Platform/SiFive/U5SeriesPkg/FreedomU540HiFiveUnleashedBoard/U540.dsc -t GCC5
-cp /Build/FreedomU540HiFiveUnleashed/DEBUG_GCC5/FV/U540.fd U540.fd
-echo Finished EDK2 build
+build -a RISCV64 -p $1 -t GCC5 \
+  && echo 'Completed EDK2 build' \
+  || echo 'Failed EDK2 build'
 
 #build -a RISCV64 -p MdeModulePkg/MdeModulePkg.dsc -t GCC5
 #build -a RISCV64 -p CryptoPkg/CryptoPkg.dsc -t GCC5
