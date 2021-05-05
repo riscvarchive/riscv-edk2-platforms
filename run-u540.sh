69d7dca784a615cf49db7bfb13b63f6a244a28fb
Daniel Schaefer <git@danielschaefer.me>
2021-05-05 14:29:33 +0800

    Run U540 and U500 build in parallel

--- run-u540.sh
+++ run-u540.sh
@@ -5,7 +5,8 @@ timeout --foreground 10 \
   -bios U540.fd \
   -m 1024 -nographic -smp cpus=5,maxcpus=5 | tee boot.log \
   || true
+ls -l boot.log
 cat boot.log
 grep -q 'Shell>' boot.log \
   && echo 'Booted to UEFI Shell' \
-  || echo 'Failed to boot to UEFI Shell'
+  || echo 'Failed to boot to UEFI Shell'; false
