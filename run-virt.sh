c76e59752cb3263fc0fccbd4cbd3909bf01ec664
Daniel Schaefer <daniel.schaefer@hpe.com>
2021-05-05 17:50:59 +0800

    Upload entire FV directory

--- run-virt.sh
+++ run-virt.sh
@@ -8,14 +8,14 @@ timeout --foreground 10 \
   || true
 
 whoami
-sudo chown $(whoami) U540.fd
-sudo chmod +rw U540.fd
-ls -l U540.fd
+chown $(whoami) Build/FreedomU540HiFiveUnleashed/DEBUG_GCC5/FV/U540.fd
+chmod +rw Build/FreedomU540HiFiveUnleashed/DEBUG_GCC5/FV/U540.fd
+ls -l Build/FreedomU540HiFiveUnleashed/DEBUG_GCC5/FV/U540.fd
 ls -l precompiled-U540.fd
 
 timeout --foreground 10 \
   qemu-system-riscv64 -machine virt \
-  -bios U540.fd \
+  -bios Build/FreedomU540HiFiveUnleashed/DEBUG_GCC5/FV/U540.fd \
   -m 1024 -nographic -smp cpus=1,maxcpus=1 | tee boot.log \
   || true
 
