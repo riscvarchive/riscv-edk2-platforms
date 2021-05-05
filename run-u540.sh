0b500bd4488ca454662d139886a7033079893399
Daniel Schaefer <git@danielschaefer.me>
2021-05-05 14:55:11 +0800

    Run in qemu after build

--- run-u540.sh
+++ run-u540.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
+set -o xtrace
 timeout --foreground 10 \
   qemu-system-riscv64 \
   -cpu sifive-u54 -machine sifive_u \
   -bios U540.fd \
   -m 1024 -nographic -smp cpus=5,maxcpus=5 | tee boot.log \
   || true
-ls -l boot.log
-cat boot.log
-grep -q 'Shell>' boot.log \
-  && echo 'Booted to UEFI Shell' \
-  || echo 'Failed to boot to UEFI Shell'; false
+grep -q 'Shell>' boot.log
