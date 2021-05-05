21c475c59e2e26cbbc015de6323be0cd77aca061
Daniel Schaefer <git@danielschaefer.me>
2021-05-05 15:15:15 +0800

    No timeout

--- run-virt.sh
+++ run-virt.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 set -o xtrace
 ls -l U540.fd
-timeout --foreground 10 \
-  qemu-system-riscv64 -machine virt \
-  -bios U540.fd \
-  -m 1024 -nographic -smp cpus=1,maxcpus=1 | tee boot.log \
-  || true
-file U540.fd
-grep -q 'Shell>' boot.log
+qemu-system-riscv64 -machine virt \
+-bios U540.fd \
+-m 1024 -nographic -smp cpus=1,maxcpus=1
