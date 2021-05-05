1ee645f513dd9c15c61bdf675ea1f89d010d8eb9
Daniel Schaefer <git@danielschaefer.me>
2021-05-05 15:23:23 +0800

    Add precompiled

--- run-virt.sh
+++ run-virt.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 set -o xtrace
+
+timeout --foreground 10 \
+  qemu-system-riscv64 -machine virt \
+  -bios precompiled-U540.fd \
+  -m 1024 -nographic -smp cpus=1,maxcpus=1 | tee boot.log \
+  || true
+
 ls -l U540.fd
-qemu-system-riscv64 -machine virt \
--bios U540.fd \
--m 1024 -nographic -smp cpus=1,maxcpus=1
+timeout --foreground 10 \
+  qemu-system-riscv64 -machine virt \
+  -bios U540.fd \
+  -m 1024 -nographic -smp cpus=1,maxcpus=1 | tee boot.log \
+  || true
+
+grep -q 'Shell>' boot.log
