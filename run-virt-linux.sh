756789aed8f7c879bed5ed9ac653cbb519f4d4a5
Daniel Schaefer <daniel.schaefer@hpe.com>
2021-05-06 19:11:52 +0800

    Remove trailing spaces

--- run-virt-linux.sh
+++ run-virt-linux.sh
@@ -4,17 +4,14 @@ set -o xtrace
 timeout --foreground 15 \
   qemu-system-riscv64 -machine virt \
   -bios Build/RiscvVirt/DEBUG_GCC5/FV/RISCVVIRT.fd \
-  -drive file=rootfs.ext2,format=raw,id=hd0 \ 
-  -device virtio-blk-device,drive=hd0 \                          
-  -drive file=esp.iso,format=raw,id=hd1 \     
-  -device virtio-blk-device,drive=hd1 \                          
-  -netdev user,id=net0 \                                         
-  -device virtio-net-device,netdev=net0 \                        
+  -drive file=rootfs.ext2,format=raw,id=hd0 \
+  -device virtio-blk-device,drive=hd0 \
+  -drive file=esp.iso,format=raw,id=hd1 \
+  -device virtio-blk-device,drive=hd1 \
+  -netdev user,id=net0 \
+  -device virtio-net-device,netdev=net0 \
   -m 1024 -nographic -smp cpus=1,maxcpus=1 | tee boot.log \
   || true
-
-  -nographic # qemu_riscv64_virt_defconfig                       
-
 
 grep -q 'Shell>' boot.log
 grep -q 'Welcome to Buildroot' boot.log
