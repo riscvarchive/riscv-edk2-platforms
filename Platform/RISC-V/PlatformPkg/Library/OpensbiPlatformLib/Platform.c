b47473029793ffde1bdbd0b8458ce3190d1e41fc
Daniel Schaefer <git@danielschaefer.me>
2021-05-12 19:34:00 +0800

    WIP: Upgrade to OpenSBI 0.10

--- Platform/RISC-V/PlatformPkg/Library/OpensbiPlatformLib/Platform.c
+++ Platform/RISC-V/PlatformPkg/Library/OpensbiPlatformLib/Platform.c
@@ -8,13 +8,11 @@
  */
 
 #include <libfdt.h>
-#include <PlatformOverride.h>
+#include <platform_override.h>
 #include <sbi/riscv_asm.h>
-#include <sbi/sbi_domain.h>
 #include <sbi/sbi_hartmask.h>
 #include <sbi/sbi_platform.h>
 #include <sbi/sbi_string.h>
-#include <sbi/sbi_math.h>
 #include <sbi_utils/fdt/fdt_domain.h>
 #include <sbi_utils/fdt/fdt_fixup.h>
 #include <sbi_utils/fdt/fdt_helper.h>
@@ -24,7 +22,10 @@
 #include <sbi_utils/ipi/fdt_ipi.h>
 #include <sbi_utils/reset/fdt_reset.h>
 
+extern const struct platform_override sifive_fu540;
+
 static const struct platform_override *special_platforms[] = {
+	&sifive_fu540,
 };
 
 static const struct platform_override *generic_plat = NULL;
@@ -134,7 +135,6 @@ static int generic_final_init(bool cold_boot)
 {
 	void *fdt;
 	int rc;
-  struct sbi_scratch *ThisScratch;
 
 	if (generic_plat && generic_plat->final_init) {
 		rc = generic_plat->final_init(cold_boot, generic_plat_match);
@@ -156,14 +156,6 @@ static int generic_final_init(bool cold_boot)
 		if (rc)
 			return rc;
 	}
-
-  //
-  // Set PMP of firmware regions to R and X. We will lock this in the end of PEI.
-  // This region only protects SEC, PEI and Scratch buffer.
-  //
-  // TODO: Can be removed when OpenSBI properly marks the FW region as RWX.
-  ThisScratch = sbi_scratch_thishart_ptr ();
-  pmp_set(0, PMP_R | PMP_X | PMP_W, ThisScratch->fw_start, log2roundup (ThisScratch->fw_size));
 
 	return 0;
 }
@@ -189,62 +181,7 @@ static u64 generic_tlbr_flush_limit(void)
 {
 	if (generic_plat && generic_plat->tlbr_flush_limit)
 		return generic_plat->tlbr_flush_limit(generic_plat_match);
-	return 0;
-}
-
-static int generic_system_reset_check(u32 reset_type, u32 reset_reason)
-{
-	if (generic_plat && generic_plat->system_reset_check)
-		return generic_plat->system_reset_check(reset_type,
-							reset_reason,
-							generic_plat_match);
-	return fdt_system_reset_check(reset_type, reset_reason);
-}
-
-static void generic_system_reset(u32 reset_type, u32 reset_reason)
-{
-	if (generic_plat && generic_plat->system_reset) {
-		generic_plat->system_reset(reset_type, reset_reason,
-					   generic_plat_match);
-		return;
-	}
-
-	fdt_system_reset(reset_type, reset_reason);
-}
-
-#define ROOT_EDK2_REGION	0
-#define ROOT_FW_REGION		1
-#define ROOT_ALL_REGION		2
-#define ROOT_END_REGION		3
-static struct sbi_domain_memregion root_memregs[ROOT_END_REGION + 1] = { 0 };
-
-struct sbi_domain_memregion *get_mem_regions(void) {
-	/* Root domain firmware memory region */
-	root_memregs[ROOT_FW_REGION].order = log2roundup(FixedPcdGet32(PcdFwEndAddress) - FixedPcdGet32(PcdFwStartAddress));
-	//root_memregs[ROOT_FW_REGION].base = scratch->fw_start & ~((1UL << root_memregs[0].order) - 1UL);
-	root_memregs[ROOT_FW_REGION].base = FixedPcdGet32(PcdFwStartAddress)
-		& ~((1UL << root_memregs[0].order) - 1UL);
-	// TODO: Why isn't this SBI_DOMAIN_MEMREGION_EXECUTABLE?
-	root_memregs[ROOT_FW_REGION].flags = 0;
-
-	root_memregs[ROOT_EDK2_REGION].order = log2roundup(FixedPcdGet32(PcdFwEndAddress) - FixedPcdGet32(PcdFwStartAddress));
-	//root_memregs[ROOT_FW_REGION].base = scratch->fw_start & ~((1UL << root_memregs[0].order) - 1UL);
-	root_memregs[ROOT_EDK2_REGION].base = FixedPcdGet32(PcdFwStartAddress)
-		& ~((1UL << root_memregs[0].order) - 1UL);
-	// TODO: Why isn't this SBI_DOMAIN_MEMREGION_EXECUTABLE?
-	root_memregs[ROOT_EDK2_REGION].flags = SBI_DOMAIN_MEMREGION_EXECUTABLE;
-
-	/* Root domain allow everything memory region */
-	root_memregs[ROOT_ALL_REGION].order = __riscv_xlen;
-	root_memregs[ROOT_ALL_REGION].base = 0;
-	root_memregs[ROOT_ALL_REGION].flags = (SBI_DOMAIN_MEMREGION_READABLE |
-						SBI_DOMAIN_MEMREGION_WRITEABLE |
-						SBI_DOMAIN_MEMREGION_EXECUTABLE);
-
-	/* Root domain memory region end */
-	root_memregs[ROOT_END_REGION].order = 0;
-
-	return root_memregs;
+	return SBI_PLATFORM_TLB_RANGE_FLUSH_LIMIT_DEFAULT;
 }
 
 const struct sbi_platform_operations platform_ops = {
@@ -252,25 +189,15 @@ const struct sbi_platform_operations platform_ops = {
 	.final_init		= generic_final_init,
 	.early_exit		= generic_early_exit,
 	.final_exit		= generic_final_exit,
-	.domains_root_regions	= get_mem_regions,
 	.domains_init		= generic_domains_init,
-	.console_putc		= fdt_serial_putc,
-	.console_getc		= fdt_serial_getc,
 	.console_init		= fdt_serial_init,
 	.irqchip_init		= fdt_irqchip_init,
 	.irqchip_exit		= fdt_irqchip_exit,
-	.ipi_send		= fdt_ipi_send,
-	.ipi_clear		= fdt_ipi_clear,
 	.ipi_init		= fdt_ipi_init,
 	.ipi_exit		= fdt_ipi_exit,
 	.get_tlbr_flush_limit	= generic_tlbr_flush_limit,
-	.timer_value		= fdt_timer_value,
-	.timer_event_stop	= fdt_timer_event_stop,
-	.timer_event_start	= fdt_timer_event_start,
 	.timer_init		= fdt_timer_init,
 	.timer_exit		= fdt_timer_exit,
-	.system_reset_check	= generic_system_reset_check,
-	.system_reset		= generic_system_reset,
 };
 
 struct sbi_platform platform = {
@@ -280,6 +207,6 @@ struct sbi_platform platform = {
 	.features		= SBI_PLATFORM_DEFAULT_FEATURES,
 	.hart_count		= SBI_HARTMASK_MAX_BITS,
 	.hart_index2id		= generic_hart_index2id,
-	.hart_stack_size	= FixedPcdGet32(PcdOpenSbiStackSize),
+	.hart_stack_size	= SBI_PLATFORM_DEFAULT_HART_STACK_SIZE,
 	.platform_ops_addr	= (unsigned long)&platform_ops
 };
