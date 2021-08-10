37fff29bd6fa461af13e3ce117407b042232a42d
Nate DeSimone <nathaniel.l.desimone@intel.com>
2021-08-10 15:38:50 -0700

    KabylakeOpenBoardPkg: Add MMIO Base/Length to SA GNVS
    The SA GNVS Area contains fields for the MMIO region
    base address and length. This implements code to
    populate those fields. The MMIO Base/Length are used
    by ASL at runtime and must be populated for normal
    system operation.
    
    Cc: Chasel Chiu <chasel.chiu@intel.com>
    Cc: Michael Kubacki <Michael.Kubacki@microsoft.com>
    Cc: Benjamin Doron <benjamin.doron00@gmail.com>
    Signed-off-by: Nate DeSimone <nathaniel.l.desimone@intel.com>
    Reviewed-by: Chasel Chiu <chasel.chiu@intel.com>
    Acked-by: Michael Kubacki <michael.kubacki@microsoft.com>

--- Platform/Intel/KabylakeOpenBoardPkg/Acpi/BoardAcpiDxe/AcpiGnvsInit.c
+++ Platform/Intel/KabylakeOpenBoardPkg/Acpi/BoardAcpiDxe/AcpiGnvsInit.c
@@ -1,7 +1,7 @@
 /** @file
   Acpi Gnvs Init Library.
 
-Copyright (c) 2017, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2017 - 2021, Intel Corporation. All rights reserved.<BR>
 SPDX-License-Identifier: BSD-2-Clause-Patent
 
 **/
@@ -11,11 +11,52 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 #include <Library/PciLib.h>
 #include <Library/DebugLib.h>
 #include <Library/BaseMemoryLib.h>
+#include <Library/UefiLib.h>
 #include <Library/UefiBootServicesTableLib.h>
 
 #include <PchAccess.h>
 #include <Protocol/GlobalNvsArea.h>
 #include <Protocol/MpService.h>
+#include <Protocol/SaGlobalNvsArea.h>
+
+/**
+  A protocol callback which updates MMIO Base and Length in SA GNVS area
+
+  @param[in] Event    - The triggered event.
+  @param[in] Context  - Context for this event.
+
+**/
+VOID
+EFIAPI
+UpdateSaGnvsForMmioResourceBaseLength (
+  IN EFI_EVENT Event,
+  IN VOID      *Context
+  )
+{
+  EFI_STATUS                            Status;
+  SYSTEM_AGENT_GLOBAL_NVS_AREA_PROTOCOL *SaGlobalNvsAreaProtocol;
+
+  Status = gBS->LocateProtocol (&gSaGlobalNvsAreaProtocolGuid, NULL, (VOID **) &SaGlobalNvsAreaProtocol);
+  if (Status != EFI_SUCCESS) {
+    return;
+  }
+  gBS->CloseEvent (Event);
+
+  //
+  // Configure MMIO Base/Length. This logic is only valid for platforms that use PciHostBridgeLibSimple.
+  //
+  DEBUG ((DEBUG_INFO, "[BoardAcpiDxe] Update SA GNVS Area.\n"));
+  SaGlobalNvsAreaProtocol->Area->Mmio32Base = PcdGet32 (PcdPciReservedMemBase);
+  if (PcdGet32 (PcdPciReservedMemLimit) != 0) {
+    SaGlobalNvsAreaProtocol->Area->Mmio32Length = PcdGet32 (PcdPciReservedMemLimit) - PcdGet32 (PcdPciReservedMemBase) + 1;
+  } else {
+    SaGlobalNvsAreaProtocol->Area->Mmio32Length = ((UINT32) PcdGet64 (PcdPciExpressBaseAddress)) - PcdGet32 (PcdPciReservedMemBase);
+  }
+  if (PcdGet64 (PcdPciReservedMemAbove4GBLimit) > PcdGet64 (PcdPciReservedMemAbove4GBBase)) {
+    SaGlobalNvsAreaProtocol->Area->Mmio64Base   = PcdGet64 (PcdPciReservedMemAbove4GBBase);
+    SaGlobalNvsAreaProtocol->Area->Mmio64Length = PcdGet64 (PcdPciReservedMemAbove4GBLimit) - PcdGet64 (PcdPciReservedMemAbove4GBBase) + 1;
+  }
+}
 
 /**
 @brief
@@ -39,6 +80,7 @@ AcpiGnvsInit (
   EFI_MP_SERVICES_PROTOCOL      *MpService;
   UINTN                         NumberOfCPUs;
   UINTN                         NumberOfEnabledCPUs;
+  VOID                          *SaGlobalNvsRegistration;
 
   Pages = EFI_SIZE_TO_PAGES (sizeof (EFI_GLOBAL_NVS_AREA));
   Address = 0xffffffff; // allocate address below 4G.
@@ -53,7 +95,7 @@ AcpiGnvsInit (
   if (EFI_ERROR(Status)) {
     return Status;
   }
-  
+
   //
   // Locate the MP services protocol
   // Find the MP Protocol. This is an MP platform, so MP protocol must be there.
@@ -90,6 +132,16 @@ AcpiGnvsInit (
   GNVS->Area->PL1LimitCS = 0;
   GNVS->Area->PL1LimitCSValue = 4500;
 
+  //
+  // Update SA GNVS with MMIO Base/Length
+  //
+  EfiCreateProtocolNotifyEvent (
+    &gSaGlobalNvsAreaProtocolGuid,
+    TPL_CALLBACK,
+    UpdateSaGnvsForMmioResourceBaseLength,
+    NULL,
+    &SaGlobalNvsRegistration
+    );
+
   return EFI_SUCCESS;
 }
-
